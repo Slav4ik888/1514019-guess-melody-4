6ca95c353d2265314b3d2176be983eb410ce942a
Vyacheslav <korzan.va@mail.ru>
2020-06-15 21:01:44 +0800

    Исправил ошибку в тесте

--- src/components/App/app.test.js
+++ src/components/App/app.test.js
@@ -2,19 +2,19 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './app';
 
-const errorsValue = [1, 4, -7, 127];
+const errorsValue = [1, null, -7, 127];
 
 describe(`index рендерит <App /> `, () => {
-  it(`index рендерит <App /> `, () => {
+  it(`index рендерит <App /> errorsValue = ${errorsValue[0]}`, () => {
     const tree = renderer
-      .create(<App errorsValue={errorsValue} />)
+      .create(<App errorsValue={errorsValue[0]} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it(`index рендерит <App /> errorsValue = null`, () => {
+  it(`index рендерит <App /> errorsValue = ${errorsValue[1]}`, () => {
     const tree = renderer
-      .create(<App errorsValue={null} />)
+      .create(<App errorsValue={errorsValue[1]} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
